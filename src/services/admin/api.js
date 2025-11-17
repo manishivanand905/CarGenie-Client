02f13889932d6b46be4232e5c7a9be361f053278
Mani Shivanand <153585297+manishivanand905@users.noreply.github.com>
2025-11-17 11:33:28 +0530

    Update BASE_URL to new server endpoint

--- src/services/admin/api.js
+++ src/services/admin/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://cargenie-server-4ftd.onrender.com";
+const BASE_URL = "https://cargenie-server-7i77.onrender.com";
 
 const api = axios.create({
   baseURL: BASE_URL,
